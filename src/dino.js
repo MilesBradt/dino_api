b931f6f3919bdafed31534ab900a0ee2dc42664d
Miles Bradt and Robert Lee <robert@>
2019-01-16 11:06:28 -0800

    add API call for dino names

--- src/dino.js
+++ src/dino.js
@@ -2,12 +2,13 @@ export class DinoSpam {
   getDinoNames() {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = 'http://dinoipsum.herokuapp.com/api/?format=htmlwords=1';
+      let url = 'http://dinoipsum.herokuapp.com/api/?format=html&paragraphs=1&words=1';
       request.onload = function() {
         if (this.status === 200) {
-        resolve(request.responce);
-      } else {
-        reject(Error(request.statusText));
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
       }
       request.open("GET", url, true);
       request.send();
--- src/main.js
+++ src/main.js
@@ -2,4 +2,20 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
-// import { Class } from './class.js';
+import { DinoSpam } from './dino.js';
+
+$(document).ready(function() {
+
+  let dinoNames = new DinoSpam();
+  let promise = dinoNames.getDinoNames();
+  console.log(promise);
+  promise.then(function(response) {
+    console.log(response);
+
+    document.getElementById("output").innerHTML = response;
+  }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+  });
+
+
+});
